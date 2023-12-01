1a9099b7ea4fd93ee899206ce5207743278fc7e9
Nir Friedman <quicknir@gmail.com>
2023-12-01 09:16:46 -0500

    Improve utility macro for reading input

--- advent_2023/src/file_utils.rs
+++ advent_2023/src/file_utils.rs
@@ -7,17 +7,19 @@ fn read_lines<P: AsRef<Path>>(filename: P) -> impl Iterator<Item = String> {
     BufReader::new(file).lines().map(|x| x.unwrap())
 }
 
-pub fn read_aoc_lines_impl(year: i16, root_dir: &str, src_filename: &str) -> impl Iterator<Item = String> {
+pub fn read_aoc_lines_impl(root_dir: &str, src_filename: &str) -> impl Iterator<Item = String> {
     let mut p = PathBuf::from(root_dir);
+    p.push("input");
     p.push(Path::new(src_filename).file_stem().unwrap());
     p.set_extension("txt");
     if !p.exists() {
-        download_file(year, &p);
+        download_file(root_dir, &p);
     }
     read_lines(p)
 }
 
-fn download_file(year: i16, target_filename: &Path) {
+fn download_file(root_dir: &str, target_filename: &Path) {
+    let year: i16 = Path::new(root_dir).file_stem().unwrap().to_str().unwrap().strip_prefix("advent_").unwrap().parse().unwrap();
     let cookie_path = target_filename.parent().unwrap().join("cookie.txt");
     let cookie = read_lines(&cookie_path).next().unwrap();
     let day: i8 = target_filename.file_stem().unwrap().to_str().unwrap().get(3..).unwrap().parse().unwrap();
@@ -30,6 +32,6 @@ fn download_file(year: i16, target_filename: &Path) {
 #[macro_export]
 macro_rules! read_aoc_lines {
     () => {
-        utils::read_aoc_lines_impl(2023, "/home/nir/Downloads/advent_2023", file!())
+        utils::read_aoc_lines_impl(env!("CARGO_MANIFEST_DIR"), file!())
     };
 }
