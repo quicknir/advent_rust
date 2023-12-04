02f37f109ac6460298037dc8f900acaee72cb563
Nir Friedman <quicknir@gmail.com>
2023-12-03 19:56:23 -0500

    Improvements for grid to help with part 3

--- advent_2023/src/grid.rs
+++ advent_2023/src/grid.rs
@@ -1,15 +1,17 @@
+use std::ops::{Index, IndexMut};
+
 use crate::Point;
 
 pub type Coord<const RANK: usize> = Point<i64, RANK>;
 
 pub struct Grid<T, const RANK: usize> {
     data: Vec<T>,
-    dims: [usize; RANK],
-    multipliers: [usize; RANK],
+    dims: [i64; RANK],
+    multipliers: [i64; RANK],
 }
 
 impl<T: Clone, const RANK: usize> Grid<T, RANK> {
-    pub fn new(t: T, dims: &[usize; RANK]) -> Grid<T, RANK> {
+    pub fn new(t: T, dims: &[i64; RANK]) -> Grid<T, RANK> {
         let mut cum_product = 1;
         let multipliers = std::array::from_fn(|i| {
             let x = cum_product;
@@ -17,17 +19,19 @@ impl<T: Clone, const RANK: usize> Grid<T, RANK> {
             x
         });
         Grid {
-            data: vec![t; RANK],
+            data: vec![t; cum_product as usize],
             dims: *dims,
             multipliers,
         }
     }
+}
 
+impl<T, const RANK: usize> Grid<T, RANK> {
     pub fn contains<U: Into<Coord<RANK>>>(&self, coord: U) -> bool {
         self.dims
             .iter()
             .zip(coord.into().as_ref())
-            .all(|(x, y)| (*y as usize) < *x)
+            .all(|(x, y)| (*y as u64) < (*x as u64))
     }
 
     pub fn get<U: Into<Coord<RANK>>>(&self, coord: U) -> Option<&T> {
@@ -37,13 +41,39 @@ impl<T: Clone, const RANK: usize> Grid<T, RANK> {
         self.convert_index(coord).map(|i| &mut self.data[i])
     }
 
+    pub fn get_dims(&self) -> [i64; RANK] {
+        self.dims
+    }
+
     fn convert_index<U: Into<Coord<RANK>>>(&self, coord: U) -> Option<usize> {
         let coord = coord.into();
         if !self.contains(coord) {
             return None;
         }
-        let dot_product = self.multipliers.into_iter().zip(coord.as_ref()).fold(0, |acc, (x, y)| acc + x * (*y as usize));
-        return Some(dot_product);
+        let dot_product = self
+            .multipliers
+            .into_iter()
+            .zip(coord.as_ref())
+            .fold(0, |acc: i64, (x, y)| acc + x * *y);
+        return Some(dot_product as usize);
+    }
+}
+
+impl<T, const RANK: usize, Idx: Into<Coord<RANK>>> Index<Idx> for Grid<T, RANK> {
+    type Output = T;
+
+    fn index(&self, index: Idx) -> &Self::Output {
+        let index = index.into();
+        self.get(index)
+            .unwrap_or_else(|| panic!("Invalid index {:?}", index))
+    }
+}
+
+impl<T, const RANK: usize, Idx: Into<Coord<RANK>>> IndexMut<Idx> for Grid<T, RANK> {
+    fn index_mut(&mut self, index: Idx) -> &mut Self::Output {
+        let index = index.into();
+        self.get_mut(index)
+            .unwrap_or_else(|| panic!("Invalid index {:?}", index))
     }
 }
 
@@ -58,6 +88,7 @@ mod tests {
         assert!(!grid.contains([3, 1, 1]));
         let coord: Coord<3> = [3, 1, 1].into();
         assert!(!grid.contains(coord));
+        assert!(!grid.contains([-1, 0, 0]));
     }
     #[test]
     fn index_map() {
@@ -66,13 +97,13 @@ mod tests {
         for i in 0..3 {
             for j in 0..4 {
                 for k in 0..5 {
-                    assert!(x.insert(grid.convert_index([i,j,k]).unwrap()));
+                    assert!(x.insert(grid.convert_index([i, j, k]).unwrap()));
                 }
             }
         }
-        assert_eq!(x.len(), 3*4*5);
+        assert_eq!(x.len(), 3 * 4 * 5);
         for e in &x {
-            assert!(*e < 3*4*5);
+            assert!(*e < 3 * 4 * 5);
         }
     }
 }
