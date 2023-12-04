c15ad04edc7d48db54dd0e71ae457837f4269dc3
Nir Friedman <quicknir@gmail.com>
2023-12-04 12:37:19 -0500

    Day 4

---
+++ advent_2023/src/bin/day4.rs
@@ -0,0 +1,75 @@
+use std::collections::VecDeque;
+use utils::*;
+
+fn count_matches(input: &str) -> usize {
+    let (_card, input) = input.split_once(": ").unwrap();
+    let (winners, have) = input.split_once(" | ").unwrap();
+    let winners: HashSet<i32> = winners
+        .split(' ')
+        .filter(|x| !x.is_empty())
+        .map(|x| x.parse().unwrap())
+        .collect();
+    have.split(' ')
+        .filter(|x| !x.is_empty())
+        .map(|x| x.parse().unwrap())
+        .filter(|x| winners.contains(x))
+        .count()
+}
+
+fn part1(input: &str) -> usize {
+    input
+        .split_terminator('\n')
+        .map(|line| {
+            let matches = count_matches(line);
+            if matches == 0 {
+                0
+            } else {
+                2usize.pow(matches as u32 - 1)
+            }
+        })
+        .sum()
+}
+
+fn part2(input: &str) -> usize {
+    let mut copies = VecDeque::new();
+    let mut total_cards = 0;
+    for line in input.split_terminator('\n') {
+        let cur_copies = copies.pop_front().unwrap_or(0) + 1;
+        total_cards += cur_copies;
+        let matches = count_matches(line);
+        if copies.len() < matches {
+            copies.resize(matches, 0);
+        }
+        for i in 0..matches {
+            copies[i] += cur_copies;
+        }
+    }
+    total_cards
+}
+
+#[cfg(test)]
+mod tests {
+    const TEST_INPUT: &str = "\
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+";
+    use crate::*;
+    #[test]
+    fn test_part1() {
+        assert_eq!(13, part1(TEST_INPUT));
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(30, part2(TEST_INPUT));
+    }
+}
+
+fn main() {
+    let s = read_aoc!();
+    println!("{:?}", part1(&s));
+    println!("{:?}", part2(&s));
+}
