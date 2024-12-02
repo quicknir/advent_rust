5279f062a0ebb80f5bc94b8a440f5ead4cbc7056
Nir Friedman <quicknir@gmail.com>
2024-12-01 22:01:53 -0500

    Cleanup solution

--- advent_2024/src/bin/day1.rs
+++ advent_2024/src/bin/day1.rs
@@ -1,14 +1,15 @@
 use utils::*;
 
-fn part1(input: &str) -> i64 {
-    let mut v1 = vec![];
-    let mut v2 = vec![];
-
-    for line in input.split_terminator("\n") {
+fn parse(input: &str) -> impl Iterator<Item=(i64, i64)> {
+    input.split_terminator('\n').map(|line| {
         let (i1, i2) = line.split_once("   ").unwrap();
-        v1.push(i1.parse::<i64>().unwrap());
-        v2.push(i2.parse::<i64>().unwrap());
-    }
+        (i1.parse().unwrap(), i2.parse().unwrap())
+    })
+}
+
+fn part1(input: &str) -> i64 {
+
+    let (mut v1, mut v2): (Vec<_>, Vec<_>) = parse(input).unzip(); 
     v1.sort();
     v2.sort();
 
@@ -20,11 +21,10 @@ fn part1(input: &str) -> i64 {
 fn part2(input: &str) -> i64 {
     let mut v1 = vec![];
     let mut h2: HashMap<i64, i64> = HashMap::new();
-    for line in input.split_terminator("\n") {
-        let (i1, i2) = line.split_once("   ").unwrap();
-        v1.push(i1.parse::<i64>().unwrap());
-        *h2.entry(i2.parse().unwrap()).or_default() += 1;
-    }
+    parse(input).for_each(|(i1, i2)| {
+        v1.push(i1);
+        *h2.entry(i2).or_default() += 1;
+    });
     v1.iter().fold(0, |acc, e| {
         acc + e * h2.get(e).unwrap_or(&0)
     })
