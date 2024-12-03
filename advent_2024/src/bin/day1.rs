527246302dbf0270489b1a0bb56ef6308fa758d6
Nir Friedman <quicknir@gmail.com>
2024-12-03 12:56:14 -0500

    Format day 1

--- advent_2024/src/bin/day1.rs
+++ advent_2024/src/bin/day1.rs
@@ -1,6 +1,6 @@
 use utils::*;
 
-fn parse(input: &str) -> impl Iterator<Item=(i64, i64)> {
+fn parse(input: &str) -> impl Iterator<Item = (i64, i64)> {
     input.split_terminator('\n').map(|line| {
         let (i1, i2) = line.split_once("   ").unwrap();
         (i1.parse().unwrap(), i2.parse().unwrap())
@@ -8,14 +8,13 @@ fn parse(input: &str) -> impl Iterator<Item=(i64, i64)> {
 }
 
 fn part1(input: &str) -> i64 {
-
-    let (mut v1, mut v2): (Vec<_>, Vec<_>) = parse(input).unzip(); 
+    let (mut v1, mut v2): (Vec<_>, Vec<_>) = parse(input).unzip();
     v1.sort();
     v2.sort();
 
-    v1.iter().zip(v2.iter()).fold(0, |acc, (i1, i2)| {
-        acc + (*i1 - *i2).abs()
-    })
+    v1.iter()
+        .zip(v2.iter())
+        .fold(0, |acc, (i1, i2)| acc + (*i1 - *i2).abs())
 }
 
 fn part2(input: &str) -> i64 {
@@ -25,9 +24,8 @@ fn part2(input: &str) -> i64 {
         v1.push(i1);
         *h2.entry(i2).or_default() += 1;
     });
-    v1.iter().fold(0, |acc, e| {
-        acc + e * h2.get(e).unwrap_or(&0)
-    })
+    v1.iter()
+        .fold(0, |acc, e| acc + e * h2.get(e).unwrap_or(&0))
 }
 
 fn main() {
