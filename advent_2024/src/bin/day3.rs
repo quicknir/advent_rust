15aeb39ea6530513e17229b07b5fb342ae5d693f
Nir Friedman <quicknir@gmail.com>
2024-12-03 21:48:31 -0500

    Do day 3 more nicely with parsing

--- advent_2024/src/bin/day3.rs
+++ advent_2024/src/bin/day3.rs
@@ -1,10 +1,30 @@
 use utils::*;
 
+enum Token {
+    Do,
+    Dont,
+    Mul(i64, i64),
+}
 
-fn part1(mut input: &str) -> i64 {
-    let mut sum = 0;
-    while let Some(i) = input.find("mul(") {
-        input = &input[(i + 4)..];
+fn parse(mut input: &str) -> Vec<Token> {
+    let mut tokens = vec![];
+
+    while !input.is_empty() {
+        if input.starts_with("do()") {
+            tokens.push(Token::Do);
+            input = &input[4..];
+            continue;
+        }
+        if input.starts_with("don't()") {
+            tokens.push(Token::Dont);
+            input = &input[7..];
+            continue;
+        }
+        if !input.starts_with("mul(") {
+            input = &input[1..];
+            continue;
+        }
+        input = &input[4..];
         let Some(end_paren) = input.find(")") else {
             continue;
         };
@@ -14,45 +34,48 @@ fn part1(mut input: &str) -> i64 {
         let (Ok(first), Ok(second)) = (first.parse::<i64>(), second.parse::<i64>()) else {
             continue;
         };
-
-        sum += first * second;
+        tokens.push(Token::Mul(first, second));
+        input = &input[end_paren..];
     }
-    sum
+    tokens
 }
 
-fn part2(input: &str) -> i64 {
-    let mut sum = 0;
-    let mut mult_enabled = true;
-    for i in 0..input.len() {
-        if input[i..].starts_with("do()") {
-            mult_enabled = true;
-            continue;
+fn part1(tokens: &[Token]) -> i64 {
+    tokens.iter().fold(0, |acc, e| {
+        let x = if let Token::Mul(x, y) = e {
+            x*y
         }
-        if input[i..].starts_with("don't()") {
-            mult_enabled = false;
-            continue;
-        }
-        if !input[i..].starts_with("mul(") || !mult_enabled {
-            continue;
-        }
-        let s = &input[(i + 4)..];
-        let Some(end_paren) = s.find(")") else {
-            continue;
+        else {
+            0
         };
-        let Some((first, second)) = s[..end_paren].split_once(',') else {
-            continue;
-        };
-        let (Ok(first), Ok(second)) = (first.parse::<i64>(), second.parse::<i64>()) else {
-            continue;
-        };
+        acc + x
+    })
+}
 
-        sum += first * second;
-    }
-    sum
+fn part2(tokens: &[Token]) -> i64 {
+    let mut mult_enabled = true;
+    tokens.iter().fold(0, |acc, e| {
+        let x = match e {
+            Token::Do => {
+                mult_enabled = true;
+                0
+            },
+            Token::Dont => {
+                mult_enabled = false;
+                0
+            },
+            Token::Mul(x, y) if mult_enabled => {
+                x * y
+            },
+            _ => 0,
+        };
+        acc + x
+    })
 }
 
 fn main() {
     let s = read_aoc!();
-    println!("{:?}", part1(&s));
-    println!("{:?}", part2(&s));
+    let parsed = parse(&s);
+    println!("{:?}", part1(&parsed));
+    println!("{:?}", part2(&parsed));
 }
