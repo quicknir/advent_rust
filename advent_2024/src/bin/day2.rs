2231f8a326003e29d063ff60f517c561ee43fd9d
Nir Friedman <quicknir@gmail.com>
2024-12-03 21:48:09 -0500

    Fix bug in day2 brute force

--- advent_2024/src/bin/day2.rs
+++ advent_2024/src/bin/day2.rs
@@ -8,7 +8,7 @@ fn parse(line: &str, storage: &mut Vec<i64>) {
 }
 
 fn adj_i(i: usize, skip_level: Option<usize>) -> usize {
-    i + ((skip_level == Some(i)) as usize)
+    i + (skip_level.map_or(false, |s| s <= i) as usize)
 }
 
 fn are_levels_safe(levels: &[i64], skip_level: Option<usize>) -> bool {
@@ -37,7 +37,7 @@ fn part2(input: &str) -> usize {
         .split_terminator('\n')
         .filter(|l| {
             parse(*l, &mut v);
-            for skip in once(None).chain((1..v.len()).map(|x| Some(x))) {
+            for skip in once(None).chain((0..v.len()).map(|x| Some(x))) {
                 if are_levels_safe(&v, skip) {
                     return true;
                 }
@@ -47,55 +47,20 @@ fn part2(input: &str) -> usize {
         .count()
 }
 
-
-fn part_two_line(levels: &[i64]) -> bool {
-    if levels.len() <= 3 {
-        todo!()
-    }
-    let mut change_counts = [0; 3];
-    for i in 0..(levels.len() - 1) {
-        let index = (levels[i + 1] - levels[i]).signum() + 1;
-        change_counts[index as usize] += 1
-    }
-    let defects = change_counts[1] + min(change_counts[0], change_counts[2]);
-    if defects == 0 {
-        return (0..(levels.len() - 1)).all(|i| (levels[i] - levels[i + 1]).abs() <= 3);
-    }
-    if defects > 1 {
-        return false;
-    }
-    let defect_val = change_counts
-        .iter()
-        .enumerate()
-        .find(|x| *x.1 == 1)
-        .unwrap()
-        .0 as i64;
-    let defect_index = (0..levels.len() - 1)
-        .find(|&i| (levels[i + 1] - levels[i]).signum() == (defect_val - 1))
-        .unwrap();
-
-    (0..(levels.len() - 2)).all(|i| {
-        let first = i + ((i >= defect_index) as usize);
-        let second = i + 1 + (((i + 1) >= defect_index) as usize);
-        (levels[first] - levels[second]).abs() <= 3
-    })
-}
-
 #[cfg(test)]
 mod tests {
     use crate::*;
-    // #[test]
-    // fn test_part1() {
-    //     assert_eq!(0, part1(&parse(TEST_INPUT)));
-    // }
+
+    const TEST_INPUT: &str = "\
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+";
     #[test]
     fn test_part2() {
-        assert_eq!(part_two_line(&[7, 6, 4, 2, 1]), true);
-        assert_eq!(part_two_line(&[1, 2, 7, 8, 9]), false);
-        assert_eq!(part_two_line(&[9, 7, 6, 2, 1]), false);
-        assert_eq!(part_two_line(&[1, 3, 2, 4, 5]), true);
-        assert_eq!(part_two_line(&[8, 6, 4, 4, 1]), true);
-        assert_eq!(part_two_line(&[1, 3, 6, 7, 9]), true);
+        assert_eq!(part2(TEST_INPUT), 2);
     }
 }
 
